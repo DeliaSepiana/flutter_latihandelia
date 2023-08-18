5943329b2fbea57bb99a517f65cc17fe593a7354
DeliaSepiana <142206551+DeliaSepiana@users.noreply.github.com>
2023-08-18 17:23:08 +0800

    Create tantangan.dart

---
+++ introduction_flutter/tantangan.dart
@@ -0,0 +1,16 @@
+//persegi panjang
+void main(){
+  int panjang = 4;
+  int lebar = 6;
+  int keliling = 2 * panjang + lebar;
+  
+  print('hasil keliling persegi panjang $keliling');
+
+//segitiga
+  int sisiA = 4;
+  int sisiB = 6;
+  int sisiC = 6;
+  int keliling_segitiga = sisiA + sisiB + sisiC;
+  
+  print('hasil keliling segitiga $keliling_segitiga');
+}
